733cc826af891bae3fd6969d95560b01b2f4dbaf
Ifeanyi-okoronkwo, Tochi <ifeanyitochipraise@gmail.com>
2025-04-03 10:08:44 +0100

    Modified the width of consult and practice pages on phone size

--- src/components/Consult/page.tsx
+++ src/components/Consult/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="relative flex flex-col justify-center z-10 w-[90%] mx-auto sm:text-center bg-white pt-16 sm:pt-0">
+    <div className="relative flex flex-col justify-center z-10 sm:text-center bg-white pt-16 sm:pt-0 px-[7%] sm:px-0">
       <h2 className="text-base sm:text-[2.2rem] font-bold text-black flex justify-center">
         CONSULT W.K. SHITTU & Co.
       </h2>
